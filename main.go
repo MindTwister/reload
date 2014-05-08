1f4d7d005a29a280193b8d195be1c8cdfc899f15
Kristoffer Sall Hansen <ksh@attention8.dk>
2014-05-08 09:44:43 +0200

    Initial

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"code.google.com/p/go.net/websocket"
+)
+
+func root(w http.ResponseWriter, r *http.Request) {
+	broadCast("Update")
+}
+
+var clients map[*websocket.Conn]struct{} = make(map[*websocket.Conn]struct{})
+var p *string
+var ping string = "ping"
+
+var script = `
+	(function(){
+		var ws = new WebSocket("ws://%v");
+		ws.onmessage = function(d) {
+			if(d.data == "Update") {
+				window.location.refresh();
+			}
+		}
+	})();
+`
+
+func keepAlive(c *websocket.Conn) {
+	for {
+		c.SetDeadline(time.Now().Add(10 * time.Second))
+		err := websocket.JSON.Send(c, ping)
+		if err != nil {
+			log.Println(err)
+			delete(clients, c)
+			break
+		}
+		time.Sleep(8 * time.Second)
+	}
+}
+
+func broadCast(msg string) {
+	for c := range clients {
+		websocket.JSON.Send(c, msg)
+	}
+}
+
+func getScript(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, script, server)
+}
+
+func ws(c *websocket.Conn) {
+	clients[c] = struct{}{}
+	keepAlive(c)
+}
+
+var server string
+
+func main() {
+	p = flag.String("port", ":8080", "-port=\":8080\"")
+	flag.Parse()
+	server = fmt.Sprintf("localhost%v", *p)
+	_, err := http.Get("http://" + server)
+	if err != nil {
+		http.Handle("/ws", websocket.Handler(ws))
+		http.HandleFunc("/script", getScript)
+		http.HandleFunc("/", root)
+		http.ListenAndServe(server, nil)
+	}
+}
